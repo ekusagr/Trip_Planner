8971f1a468036920ae515c5483a7a79f21754cec
Kushagra Agrawal <kushagra101010@gmail.com>
2026-04-09 10:27:36 +0530

    Added changes

---
+++ agent/agentic_wrokflow_api.py
@@ -0,0 +1,39 @@
+from utils.model_loader import ModelLoader
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from prompt_library.system_prompt import SYSTEM_PROMPT
+
+class GraphBuilder():
+    def __init__(self, model_provider:str="groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        self.tools = []
+        self.weather_tools = WeatherInfoTool()
+        self.tools = []
+        self.tools.extend([* self.weather_tools.weather_tool_list])
+
+        self.system_prompt=SYSTEM_PROMPT
+        self.graph = None
+        
+    def agent_function(self, state:MessagesState):
+        user_question=state["messages"]
+        input_question=[self.system_prompt]+ user_question
+        response= self.llm.invoke(input_question)
+        return {"messages": [response]}
+
+    def build_graph(self):
+        graph_builder=StateGraph(MessagesState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent",tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent",END)
+        self.graph = graph_builder.compile()
+        return self.graph
+
+    def __call__(self):
+        return self.build_graph()
+
